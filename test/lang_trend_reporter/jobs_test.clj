0931d3c2e2649f3e584edad7c4d73a20c9fa7717
Nick Miano <nmiano84@gmail.com>
2015-08-01 19:20:07 -0400

    spec for job conversion function

---
+++ test/lang_trend_reporter/jobs_test.clj
@@ -0,0 +1,22 @@
+(ns lang-trend-reporter.jobs-test
+  (:require [clojure.test :refer :all]
+            [lang-trend-reporter.jobs-http :as jobs]))
+
+(def job-before
+  {:id            "47bfd166-36f8-11e5-8403-91cec3b60979"
+   :created_at    "Thu Jul 30 20:19:50 UTC 2015"
+   :title         "Software Engineer (Robotics/Mechatronics)"
+   :location      "Boston, MA"
+   :type          "Full Time"
+   :description   "This is a job that requires Python experience."
+   :how_to_apply  "Fill out an application"
+   :company       "Formlabs"
+   :company_url   "http://formlabs.com"
+   :company_logo  "http://github-jobs.s3.amazonaws.com/411faec6-36f8-11e5-9989-ee15c863d832.jpg"
+   :url           "http://jobs.github.com/positions/47bfd166-36f8-11e5-8403-91cec3b60979"})
+
+(def job-after {:title       "software engineer (robotics/mechatronics)"
+                :description "this is a job that requires python experience."})
+(deftest a-test
+  (testing ""
+    (is (= (jobs/get-desc-and-title job-before) job-after))))
