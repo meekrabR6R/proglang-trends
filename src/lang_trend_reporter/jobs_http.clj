826ca7b3e7edfdbd8d8230ada5ff22717e1e044f
Nick Miano <nmiano84@gmail.com>
2015-08-01 18:55:27 -0400

    add vector of programming languages

--- src/lang_trend_reporter/jobs_http.clj
+++ src/lang_trend_reporter/jobs_http.clj
@@ -1,7 +1,16 @@
 (ns lang-trend-reporter.jobs-http
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client]
+            [clojure.string  :as string]))
 
-(def base-url "https://jobs.github.com/positions.json?location=")
+(def ^{:private true} base-url
+  "https://jobs.github.com/positions.json?location=")
+
+(def ^{:private true} langs
+  ["python" "javascript"   "ruby"   "scala"  "clojure" "c"
+   "c++"    "erlang"       "node"   "java"   "groovy"  "lua"
+   "swift"  "objective-c"  "php"    "rust"   "golang"  "c#"
+   "f#"     ".net"         "sql"    "perl"   "haskell" "scheme"
+   "lisp"   "visual basic" "matlab"])
 
 (defn get-jobs-by-city [city]
   "returns a vector of available jobs by city"
