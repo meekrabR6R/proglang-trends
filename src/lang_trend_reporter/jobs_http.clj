aa57a4421ff1160c2a08aa37cb0e5d656996e894
Nick Miano <nmiano84@gmail.com>
2015-08-01 19:19:50 -0400

    function to convert job map to a map only containing 'description' and 'title' fields with text downcased

--- src/lang_trend_reporter/jobs_http.clj
+++ src/lang_trend_reporter/jobs_http.clj
@@ -12,7 +12,24 @@
    "f#"     ".net"         "sql"    "perl"   "haskell" "scheme"
    "lisp"   "visual basic" "matlab"])
 
-(defn get-jobs-by-city [city]
+(defn get-desc-and-title
+  "takes a job map and returns a new map of only the
+  description and title since they are the only fields
+  needed. all text is downcased"
+  [{desc :description title :title}]
+  {:title       (string/lower-case title)
+   :description (string/lower-case desc)})
+
+(defn get-jobs-by-city
   "returns a vector of available jobs by city"
+  [city]
   (let [jobs-json (client/get (str base-url city) {:as :json})]
     (vec (:body jobs-json))))
+
+(defn transform-jobs-vec
+  "converts each item in jobs vec to new map with
+  only 'description' and 'title' fields with all text downcased"
+  [jarbs]
+  (vec (map #(get-desc-and-title %) jarbs)))
+
+
