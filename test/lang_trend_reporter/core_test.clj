35f6d060a7847cacafa9a8e95d9de5c96c45ac92
Nick Miano <nmiano84@gmail.com>
2015-08-02 17:49:30 -0400

    remove unneeded core spec

--- test/lang_trend_reporter/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns lang-trend-reporter.core-test
-  (:require [clojure.test :refer :all]
-            [lang-trend-reporter.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
