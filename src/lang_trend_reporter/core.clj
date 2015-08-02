3e2db385e6927b5df71e8be4d5b6b84d40617fa0
Nick Miano <nmiano84@gmail.com>
2015-08-02 17:49:19 -0400

    pring lang trend strings by city

--- src/lang_trend_reporter/core.clj
+++ src/lang_trend_reporter/core.clj
@@ -1,7 +1,15 @@
 (ns lang-trend-reporter.core
+  (:require [lang-trend-reporter.jobs :as job])
   (:gen-class))
 
 (defn -main
-  "Runs program"
+  "Prints breakdowns of programming language popularity by city
+   based on references in jobs listings."
   [& args]
-  (println "Hello, World!"))
+  (println (job/format-lang-trends-str "boston "))
+  (println (job/format-lang-trends-str "san francisco"))
+  (println (job/format-lang-trends-str "los angeles"))
+  (println (job/format-lang-trends-str "denver"))
+  (println (job/format-lang-trends-str "boulder"))
+  (println (job/format-lang-trends-str "chicago")))
+
