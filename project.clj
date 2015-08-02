0201f3c6d513a4abc7e5072144abc1bedf3e824c
Nick Miano <nmiano84@gmail.com>
2015-08-02 17:47:27 -0400

    add json dep

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject lang-trend-reporter "0.1.0-SNAPSHOT"
   :description "Reports programming language trends in various US cities"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-http "2.0.0"]]
+                 [clj-http "2.0.0"]
+                 [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot lang-trend-reporter.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
